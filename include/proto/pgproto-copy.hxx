66b58b0d6dc505d3ed1a8f415ca112cf1f06cd71
Bernd Helmle <bernd.helmle@credativ.de>
2020-03-16 16:56:46 +0100

    Implement a skeleton class hierarchy for objects representing COPY.
    These classes implement specific stages for the PostgreSQL COPY sub-protocol.
    
    Nothing usable yet, commit to keep things in shape.

--- include/proto/pgproto-copy.hxx
+++ include/proto/pgproto-copy.hxx
@@ -10,6 +10,14 @@ using namespace credativ::pgprotocol;
 
 namespace credativ {
 
+  /* Forwarded declarations */
+  class PGProtoCopyFormat;
+  class PGProtoCopyFail;
+  class PGProtoCopyDone;
+  class PGProtoCopyIn;
+  class PGProtoCopyOut;
+  class PGProtoCopyBoth;
+
   /**
    * Copy subprotocol exception class.
    */
@@ -31,6 +39,72 @@ namespace credativ {
   };
 
   /**
+   * COPY format identifier.
+   */
+  typedef enum {
+                COPY_TEXT = 0,
+                COPY_BINARY = 1
+  } PGProtoCopyFormatType;
+
+  /**
+   * Copy format instruction class.
+   */
+  class PGProtoCopyFormat {
+  protected:
+
+    short *formats = nullptr;
+    unsigned short cols = 0;
+    PGProtoCopyFormatType copy_format_type = COPY_BINARY;
+
+  public:
+
+    /*
+     * Default c'tor, make a copy format
+     * with the number of columns and either
+     * all of them set to textual (all_binary=false)
+     * or binary format (all_binary=true).
+     */
+    PGProtoCopyFormat(unsigned short num_cols = 1,
+                      bool all_binary = true);
+
+    virtual ~PGProtoCopyFormat();
+
+    /**
+     * Number of columns in COPY response.
+     */
+    virtual unsigned int count();
+
+    /**
+     * Pointer to the column format array. Don't modify
+     * the pointer directly, since this is managed by the
+     * format instance itself.
+     */
+    virtual short * ptr();
+
+
+    /**
+     * Get the format identifier for the specified column.
+     */
+    virtual short get(unsigned short idx);
+
+    /**
+     * Set the format specified for the specified column.
+     */
+    virtual void  set(unsigned short idx, short value);
+    virtual short operator[](unsigned short idx);
+
+    /**
+     * Set the overall COPY mode, either COPY_TEXT or COPY_BINARY.
+     */
+    virtual void setFormat(PGProtoCopyFormatType format_type);
+
+    /**
+     * Returns the current overall format identifier.
+     */
+    virtual PGProtoCopyFormatType getFormat();
+  };
+
+  /**
    * Base class for PostgreSQL COPY sub-protocol
    * implementations.
    */
@@ -38,6 +112,28 @@ namespace credativ {
   protected:
 
     ProtocolBuffer *buf = nullptr;
+    PGMessageType type = '\0';
+
+    /**
+     * Overall size of the copy message,
+     * including 4 byte message size indicator.
+     *
+     * NOTE: derived copy format classes don't modify
+     *       this attribute directly, they implement an
+     *       own overwritten method calculateSize()
+     *       to recalculate the message size.
+     */
+    std::size_t size = 0;
+
+    /*
+     * Prepare a copy message start
+     */
+    virtual void prepare();
+
+    /**
+     * Calculate total message size.
+     */
+    virtual std::size_t calculateSize() = 0;
 
   public:
 
@@ -49,14 +145,106 @@ namespace credativ {
 
   };
 
+  /**
+   * Implements COPY FROM protocol support.
+   */
   class PGProtoCopyIn : public PGProtoCopy {
+  private:
+
+    PGProtoCopyFormat format;
+
+  protected:
+
+    size_t calculateSize();
+    
   public:
 
-    PGProtoCopyIn(ProtocolBuffer *buf);
+    PGProtoCopyIn(ProtocolBuffer *buf, const PGProtoCopyFormat &format);
     virtual ~PGProtoCopyIn();
 
     virtual void begin();
+    virtual void data(ProtocolBuffer *buf);
     virtual void end();
+
+  };
+
+  /**
+   * Implements COPY TO protocol support.
+   */
+  class PGProtoCopyOut : public PGProtoCopy {
+  public:
+
+    PGProtoCopyOut(ProtocolBuffer *buf);
+    virtual ~PGProtoCopyOut();
+
+    virtual void begin();
+    virtual void data(ProtocolBuffer *buf);
+    virtual void end();
+
+  };
+
+  /**
+   * CopyBothResponse sub-protocol handler, used by START_REPLICATION
+   * commands.
+   */
+  class PGProtoCopyBoth: public PGProtoCopy {
+  public:
+
+    PGProtoCopyBoth(ProtocolBuffer *buf);
+    virtual ~PGProtoCopyBoth();
+
+    virtual void begin();
+    virtual void data(ProtocolBuffer *buf);
+    virtual void end();
+  };
+
+  /**
+   * Constructs a CopyFail Message
+   */
+  class PGProtoCopyFail : public PGProtoCopy {
+  private:
+
+    std::string error_message = "";
+
+  protected:
+
+    virtual std::size_t calculateSize();
+
+  public:
+
+    PGProtoCopyFail(ProtocolBuffer *buf);
+    virtual ~PGProtoCopyFail();
+
+    virtual void begin();
+    virtual void message(std::string msg);
+    virtual void end();
+
+  };
+
+  /**
+   * Constructs a CopyDone message.
+   */
+  class PGProtoCopyDone : public PGProtoCopy {
+  protected:
+
+    virtual std::size_t calculateSize();
+
+  public:
+
+    PGProtoCopyDone(ProtocolBuffer *buf);
+    virtual ~PGProtoCopyDone();
+
+    virtual void begin();
+    virtual void end();
+
+  };
+
+  /**
+   * Constructs a CopyData message.
+   */
+  class PGProtoCopyData : public PGProtoCopy {
+  public:
+
 
   };
 
--- src/protocol/pgproto-copy.cxx
+++ src/protocol/pgproto-copy.cxx
@@ -1,30 +1,322 @@
+#include <sstream>
 #include <ostream>
 #include <pgproto-copy.hxx>
+
+/* *****************************************************************************
+ * Base class PGProtoCopyFormat
+ * *****************************************************************************/
+
+PGProtoCopyFormat::PGProtoCopyFormat(unsigned short num_cols,
+                                     bool all_binary) {
+
+  this->cols = num_cols;
+  this->formats = new short[this->cols];
+
+  /*
+   * PostgreSQL COPY protocol requires all
+   * format flags to be textual in case the overal
+   * format flag is set to textual, too. We do the same
+   * for the binary format, though this can be overwritten
+   * in this case. See set() for details.
+   */
+  if (all_binary) {
+
+    for(int i = 0;i < cols; i++)
+      formats[i] = COPY_BINARY;
+
+    copy_format_type = COPY_BINARY;
+
+  } else {
+
+    for(int i = 0;i < cols; i++)
+      formats[i] = COPY_TEXT;
+
+    copy_format_type = COPY_TEXT;
+
+  }
+}
+
+PGProtoCopyFormat::~PGProtoCopyFormat() {
+
+  delete formats;
+
+}
+
+PGProtoCopyFormatType PGProtoCopyFormat::getFormat() {
+
+  return copy_format_type;
+
+}
+
+void PGProtoCopyFormat::setFormat(PGProtoCopyFormatType format_type) {
+
+  copy_format_type = format_type;
+
+  if (copy_format_type == 0) {
+
+    /* Text format requires column format
+     * to be textual as well */
+    for (int i = 0; i < cols; i++)
+      formats[i] = 0;
+
+  } else if (copy_format_type == 1) {
+
+    /* nothing special needs to be done here */
+
+  } else {
+
+    std::ostringstream err;
+
+    err << "invalid copy format type: \"" << format_type << "\"";
+    throw CopyProtocolFailure(err.str());
+    
+  }
+}
+
+unsigned int PGProtoCopyFormat::count() {
+
+  return cols;
+
+}
+
+short * PGProtoCopyFormat::ptr() {
+
+  return formats;
+
+}
+
+short PGProtoCopyFormat::get(unsigned short idx) {
+
+  if (idx > (cols - 1))
+    throw CopyProtocolFailure("invalid access to copy format header");
+
+  return formats[idx];
+
+}
+
+void PGProtoCopyFormat::set(unsigned short idx,
+                            short value) {
+
+  if (idx > (cols - 1))
+    throw CopyProtocolFailure("invalid access to copy format header");
+
+  formats[idx] = value;
+
+}
+
+short PGProtoCopyFormat::operator[](unsigned short idx) {
+
+  return get(idx);
+
+}
 
 /* *****************************************************************************
  * Base class PGProtoCopy
  * *****************************************************************************/
 
-PGProtoCopy::PGProtoCopy(ProtocolBuffer *buf) { }
+PGProtoCopy::PGProtoCopy(ProtocolBuffer *buf) {
+
+  /* We need a valid reference to recv/send buffer
+   * handler */
+  if (buf == nullptr) {
+    throw CopyProtocolFailure("undefined recv/send buffer handler");
+  }
+
+  this->buf = buf;
+
+}
 
 
 PGProtoCopy::~PGProtoCopy() {}
+
+void PGProtoCopy::prepare() {
+
+  /* Calculate overal message size */
+  calculateSize();
+
+  /* Adjust buffer for message, including type byte */
+  buf->allocate(size + 1);
+
+  /*
+   * Prepare the message type byte.
+   */
+  buf->write_byte(type);
+
+  /*
+   * Set message size
+   */
+  buf->write_int(size);
+
+
+}
+
+/* *****************************************************************************
+ * Implementation of class PGProtoCopyDone
+ * *****************************************************************************/
+
+PGProtoCopyDone::PGProtoCopyDone(ProtocolBuffer *buf)
+  : PGProtoCopy(buf) {
+
+  type = CopyDoneMessage;
+
+}
+
+PGProtoCopyDone::~PGProtoCopyDone() {}
+
+void PGProtoCopyDone::begin() {
+
+  /* A CopyDone message is  relatively simple,
+   * just call prepare to materialize the message header
+   * and we're done */
+  prepare();
+
+}
+
+void PGProtoCopyDone::end() {}
+
+std::size_t PGProtoCopyDone::calculateSize() {
+
+  size = 0;
+  size += sizeof(MESSAGE_HDR_LENGTH_SIZE);
+
+  return size;
+
+}
+
+/* *****************************************************************************
+ * Implementation of class PGProtoCopyFail
+ * *****************************************************************************/
+
+PGProtoCopyFail::PGProtoCopyFail(ProtocolBuffer *buf)
+  : PGProtoCopy(buf) {
+
+  type = CopyFailMessage;
+
+}
+
+PGProtoCopyFail::~PGProtoCopyFail() {}
+
+void PGProtoCopyFail::begin() {
+
+  /* Prepare message header */
+  prepare();
+
+  /*
+   * Write error message, including null byte
+   *
+   * NOTE: calculateSize() already honored the trailing
+   *       NULL byte.
+   */
+  buf->write_buffer(error_message.c_str(), error_message.length());
+  buf->write_byte('\0');
+
+}
+
+void PGProtoCopyFail::message(std::string msg) {
+
+  error_message = msg;
+
+}
+
+void PGProtoCopyFail::end() {
+  /* no op here */
+}
+
+std::size_t PGProtoCopyFail::calculateSize() {
+
+  size = 0;
+  size += sizeof(MESSAGE_HDR_LENGTH_SIZE);
+  size += (error_message.length() + 1); /* null byte */
+
+  return size;
+
+}
 
 /* *****************************************************************************
  * PGProtoCopyIn, protocol handler for COPY FROM STDIN
  * *****************************************************************************/
 
-PGProtoCopyIn::PGProtoCopyIn(ProtocolBuffer *buf)
-  : PGProtoCopy(buf) {}
+PGProtoCopyIn::PGProtoCopyIn(ProtocolBuffer *buf,
+                             const PGProtoCopyFormat &format)
+  : PGProtoCopy(buf) {
+
+  type = CopyInResponseMessage;
+  this->format = format;
+
+}
 
 PGProtoCopyIn::~PGProtoCopyIn() {}
 
+size_t PGProtoCopyIn::calculateSize() {
+
+  /* size of message length property */
+  size += sizeof(MESSAGE_HDR_LENGTH_SIZE);
+
+  /* format identifier */
+  size += sizeof(char);
+
+  /* number of cols property */
+  size += sizeof(short);
+
+  /* size of format identifier, for <cols> columns */
+  size += (sizeof(short) * format.count());
+
+  return size;
+  
+}
+
 void PGProtoCopyIn::begin() {
 
-  /*
-   * Create a CopyInResponse message to be sent to the client.
-   */
+  /* Prepare message header */
+  prepare();
+
+  /* Write message format identifier */
+  buf->write_byte((char)format.getFormat());
+  
+}
+
+void PGProtoCopyIn::data(ProtocolBuffer *buf) {
 
 }
 
 void PGProtoCopyIn::end() {}
+
+/* *****************************************************************************
+ * PGProtoCopyOut, protocol handler for COPY TO STDOUT
+ * *****************************************************************************/
+
+PGProtoCopyOut::PGProtoCopyOut(ProtocolBuffer *buf)
+  : PGProtoCopy(buf) {
+
+  type = CopyOutResponseMessage;
+
+}
+
+PGProtoCopyOut::~PGProtoCopyOut() {}
+
+void PGProtoCopyOut::begin() {
+
+}
+
+void PGProtoCopyOut::end() {}
+
+void PGProtoCopyOut::data(ProtocolBuffer *buf) {}
+
+/* *****************************************************************************
+ * PGProtoCopyBoth, protocol handler for START_REPLICATION Copy sub-protocol
+ * *****************************************************************************/
+
+PGProtoCopyBoth::PGProtoCopyBoth(ProtocolBuffer *buf)
+  : PGProtoCopy(buf) {
+
+  type = CopyBothResponseMessage;
+
+}
+
+PGProtoCopyBoth::~PGProtoCopyBoth() {}
+
+void PGProtoCopyBoth::begin() {}
+
+void PGProtoCopyBoth::data(ProtocolBuffer *buf) {}
+
+void PGProtoCopyBoth::end() {}
