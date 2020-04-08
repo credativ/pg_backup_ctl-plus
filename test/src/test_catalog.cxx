3a640964fd7604ac77b67680aac18318f4208cd7
Bernd Helmle <bernd.helmle@credativ.de>
2020-04-08 12:59:03 +0200

    More unit testing for BackupCatalog.

--- test/src/test_catalog.cxx
+++ test/src/test_catalog.cxx
@@ -29,7 +29,7 @@ BOOST_AUTO_TEST_CASE(TestBackupCatalogSetup)
 
 }
 
-BOOST_AUTO_TEST_CASE(TestBackupCatalogManip)
+BOOST_AUTO_TEST_CASE(TestBackupCatalogCreateArchive)
 {
 
   std::shared_ptr<BackupCatalog> catalog = nullptr;
@@ -44,27 +44,70 @@ BOOST_AUTO_TEST_CASE(TestBackupCatalogManip)
   /* 3 Backup catalog should be available */
   BOOST_CHECK( catalog->available() );
 
-  /* 4 Create archive "test" */
+  /*
+   * 4 Create archive "test"
+   *
+   * NOTE: BackupCatalog::createArchive doesn't require
+   *                      pushAffectedAttributes() anymore,
+   *                      since we always need the same number
+   *                      of attributes here.
+   */
   {
     std::shared_ptr<CatalogDescr> desc = std::make_shared<CatalogDescr>();
     std::shared_ptr<CatalogDescr> check_desc;
 
-    /*
-     * Set connection type. this is important since BackupCatalog::createArchive()
-     * insists this to be set!
-     */
-    desc->coninfo->type = ConnectionDescr::CONNECTION_TYPE_BASEBACKUP;
-    desc->archive_name = "test";
-    desc->directory = "/tmp";
-    desc->compression = false;
-
     BOOST_REQUIRE_NO_THROW( catalog->startTransaction() );
 
+
+    /* CatalogDescr::archive_name required, should throw */
+    BOOST_CHECK_THROW( catalog->createArchive(desc),
+                       CCatalogIssue );
+
+    desc->archive_name = "test";
+
+    /* CatalogDescr::directory required, should throw */
+    BOOST_CHECK_THROW( catalog->createArchive(desc),
+                       CCatalogIssue );
+
+    desc->directory = "/tmp";
+
+    /*
+     * Set connection type. this is important since BackupCatalog::createArchive()
+     * insists this to be set! Thus, this should throw.
+     */
+    BOOST_CHECK_THROW( catalog->createArchive(desc),
+                       CCatalogIssue );
+    desc->coninfo->type = ConnectionDescr::CONNECTION_TYPE_BASEBACKUP;
+
+    /* Should succeed now */
+    /* Force compression off, though not required */
+    desc->compression = false;
     BOOST_REQUIRE_NO_THROW( catalog->createArchive(desc) );
 
     /* Check whether archive exists */
     BOOST_REQUIRE_NO_THROW( check_desc = catalog->existsByName("test") );
     BOOST_CHECK( check_desc->id != -1 );
+    BOOST_TEST( check_desc->id > -1 );
+    BOOST_CHECK_EQUAL( check_desc->archive_name, "test" );
+    BOOST_CHECK_EQUAL( check_desc->directory, "/tmp" );
+    BOOST_CHECK_EQUAL( check_desc->compression, false );
+
+    /* Create a basebackup streaming connection */
+
+    /* This must fail, since no connection DSN/attributes currently set */
+    BOOST_CHECK_THROW( catalog->createCatalogConnection(check_desc->coninfo),
+                       CCatalogIssue );
+
+    /* Assign a DSN string and attributes, this should succeed now */
+    check_desc->coninfo->pushAffectedAttribute(SQL_CON_DSN_ATTNO);
+    check_desc->coninfo->pushAffectedAttribute(SQL_CON_ARCHIVE_ID_ATTNO);
+    check_desc->coninfo->pushAffectedAttribute(SQL_CON_TYPE_ATTNO);
+
+    check_desc->coninfo->dsn = "host=bar.server.name dbname=foo user=test";
+    check_desc->coninfo->archive_id = check_desc->id;
+    check_desc->coninfo->type = ConnectionDescr::CONNECTION_TYPE_BASEBACKUP;
+
+    BOOST_REQUIRE_NO_THROW( catalog->createCatalogConnection(check_desc->coninfo) );
 
     /* Drop archive and recheck */
     BOOST_REQUIRE_NO_THROW( catalog->dropArchive("test") );
